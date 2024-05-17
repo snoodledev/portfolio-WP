e17ca1424a2115c055b1f0e6b47cd89fecfa0b62
snoodle <nicholasduncan37@gmail.com>
2024-05-17 22:14:13 +1000

    test update

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-887.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'DOMContentLoaded', function() {
+	UAGBInlineNotice.init( {"c_id":null,"cookies":false,"close_cookie_days":1,"noticeDismiss":"","icon":"rectangle-xmark"}, '.uagb-block-c7349bd4' );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"d1da7f7c","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":false,"afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-d1da7f7c', 887 );
+});
+ });
