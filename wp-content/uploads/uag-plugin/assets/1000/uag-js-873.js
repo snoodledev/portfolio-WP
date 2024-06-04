42c46330454d8848300ad760581d3a276d578b62
snoodle <nicholasduncan37@gmail.com>
2024-06-05 01:30:50 +1000

    primary update

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-873.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-873.js
@@ -1,4 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-a9e270fb' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-e7bcbc69' );
+});
+window.addEventListener("DOMContentLoaded", function(){
 	UAGBForms.init( {"block_id":"d1da7f7c","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"nicholasduncan37@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-d1da7f7c', 873 );
 });
  });
