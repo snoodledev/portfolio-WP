8ecd9fb56f49aac82352496cc2743c252da5f842
snoodle <nicholasduncan37@gmail.com>
2024-06-05 02:17:31 +1000

    tweaks

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-873.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-873.js
@@ -5,6 +5,6 @@ window.addEventListener( 'load', function() {
 	UAGBButtonChild.init( '.uagb-block-e7bcbc69' );
 });
 window.addEventListener("DOMContentLoaded", function(){
-	UAGBForms.init( {"block_id":"d1da7f7c","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"nicholasduncan37@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-d1da7f7c', 873 );
+	UAGBForms.init( {"block_id":"d1da7f7c","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":false,"afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-d1da7f7c', 873 );
 });
  });
